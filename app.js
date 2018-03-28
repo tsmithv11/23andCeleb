76783ce4adf8f0a48f3026e4c975a6a2e93a3ac1
Taylor <28880387+tsmithv11@users.noreply.github.com>
2018-03-28 01:28:26 +0000

    Add upload path

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-var express = require("express"),
-    app     = express();
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    fs = require("fs"),
+    multer = require("multer"),
+    upload = multer({ dest: '/tmp' });
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 
 app.get("/", function(req, res){
     res.render("index");
+});
+
+
+app.post('/upload', upload.single('file'), function(req, res) {
+  var file = __dirname + '/' + req.file.filename;
+  fs.rename(req.file.path, file, function(err) {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    } else {
+      res.json({
+        message: 'File uploaded successfully',
+        filename: req.file.filename
+      });
+    }
+  });
 });
 
 
